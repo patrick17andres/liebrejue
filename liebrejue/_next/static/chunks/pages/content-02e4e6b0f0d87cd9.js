8f1eed9fb01ccd3dda528c41f67ecfd11b6d7ec5
Patricio Negrete <pnegrete@mma.gob.cl>
2022-10-12 16:50:49 -0300

    raiz y carpeta liebre jue con assetPrefix

---
+++ liebrejue/_next/static/chunks/pages/content-02e4e6b0f0d87cd9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[163],{2666:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/content",function(){return u(7894)}])}},function(n){n.O(0,[675,188,894,774,888,179],(function(){return _=2666,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
