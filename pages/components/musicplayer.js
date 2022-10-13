b279795f2af5d3ac87425c92c16b835986fd47c6
Patricio Negrete <pnegrete@mma.gob.cl>
2022-10-13 14:13:50 -0300

    funciona

--- pages/components/musicplayer.js
+++ pages/components/musicplayer.js
@@ -55,14 +55,7 @@ export default function PlayerMusic() {
   }
   return (
     <div className='text-white'>
-      <ReactJkMusicPlayer
-        className='float-none'
-        {...options}
-        audioLists={audioList}
-        mobileMediaQuery="(max-width: 1024px)"
-        showMediaSession
 
-      />
     </div>
   )
 }
